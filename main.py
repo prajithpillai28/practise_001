11dbca0307f1ae555541262bc7153cb7465cbbfd
Prajith Pillai <Prajith.Pillai@gevernova.com>
2025-04-11 14:48:01 +0530

    Added exploratory data analysis
    EDA including feature engineering and visualisation

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+class ML_algorithms:
+
+    def __init__(self,data):
+
+        self.data=data
+        ML_algorithms.split(self, data)
+
+    def split(self,data):
+        X_train,X_val=train_test_split(data)
+
+    def logistic(self,Xtrain):
+        y_train=LogisticRegression(Xtrain)
+        return y_train
+
